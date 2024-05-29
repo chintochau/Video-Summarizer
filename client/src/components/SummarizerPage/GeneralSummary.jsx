fc0fde6d06d791934e530b0ca71ede49c8fb609d
Jason Chau <what.chau@gmail.com>
2024-05-29 00:35:44 -0400

    update summarizer page

--- client/src/components/SummarizerPage/GeneralSummary.jsx
+++ client/src/components/SummarizerPage/GeneralSummary.jsx
@@ -11,7 +11,7 @@ import VideoField from "../summarizerComponents/YTVideoField";
 import { useVideoContext } from "@/contexts/VideoContext";
 import { useAuth } from "@/contexts/AuthContext";
 import { useNavigate } from 'react-router-dom';
-import { Loader2 } from 'lucide-react'
+import { ArrowRight, Loader2 } from 'lucide-react'
 import { Separator } from "../ui/separator";
 import { Button } from "../ui/button";
 import { Helmet } from "react-helmet-async";
@@ -44,7 +44,7 @@ const GeneralSummary = () => {
         <meta name="twitter:description" content="Use Fusion AI's free YouTube video summarizer to quickly get concise summaries. Try it now with a free quota and register for more features!"/>
         <link rel="canonical" href={`${fusionaiLink}/summarizer`}/>
       </Helmet>
-        <div className="w-full px-2 py-10 md:py-20">
+        <div className="w-full px-2 pt-16 pb-20 md:py-40">
           <div className="flex flex-col justify-center mx-auto max-w-3xl w-full  ">
             <div className="flex flex-col items-center justify-center">
             <h1 className="w-full text-left md:text-center text-3xl font-extrabold leading-8 text-cyan-700/70 sm:text-4xl sm:leading-10 ">
@@ -56,18 +56,22 @@ const GeneralSummary = () => {
             </div>
             <div className=" my-4"><YoutubeBar className="outline outline-1 rounded-lg outline-cyan-700/70" /></div>
           <p
-          className="w-full px-2  text-left text-sm md:text-lg text-gray-400 font-roboto font-normal max-w-3xl mx-auto"
+          className="w-full px-1  text-left text-sm md:text-lg text-gray-400 font-roboto font-normal max-w-3xl mx-auto"
           >
           Simply input the video link, and let our advanced AI generate a precise summary for you. <br className="hidden"/>Save time and get straight to the point with Fusion AI.
           </p>
-            <div className="text-left">
-              <Separator className="mb-8 mt-20" />
-              <p
-                className="text-gray-400 text-sm md:text-lg px-1 pb-4"
-              >
+          <p 
+          className="w-full px-1  text-left text-sm  text-gray-400 font-roboto font-normal max-w-3xl mx-auto py-2"
+          >
+                * Current we support GPT-3.5, GPT-4o, Claude3 and Llama3 models.
+              </p>
+            <div className="text-right">
+              <Separator className="mb-8 mt-16" />
+              <p className="text-gray-400 text-sm md:text-lg px-1 pb-4 text-left" >
               Want unlimited access and advanced features? Log in now to unlock the full potential of Fusion AI, including more summaries, cloud storage, and personalized video knowledge database.
               </p>
-              <Button variant="outline" onClick={() => navigate("/login")}>Sign in</Button>
+
+              <Button variant="link" onClick={() => navigate("/login")} className="mx-auto text-cyan-600">Sign in now<ArrowRight /></Button>
             </div>
           </div>
         </div>
