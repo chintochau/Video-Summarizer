fbed83ca908ae647245b8bd58411de4dc4f5f517
Jason Chau <what.chau@gmail.com>
2024-07-21 13:03:20 -0400

    update share link

--- server-2/utils/ssrUtils.js
+++ server-2/utils/ssrUtils.js
@@ -1,4 +1,7 @@
 // Generate meta tags based on the page content
+
+const shareLink = process.env.SHARE_URL
+
 export function generateMetaTags(summary) {
   return `
     <title>${summary.sourceTitle} | Youtube Summary</title>
@@ -6,11 +9,11 @@ export function generateMetaTags(summary) {
       <meta property="og:title" content="${summary.sourceTitle}" />
       <meta property="og:description" content="${summary.summary}" />
       <meta property="og:image" content="https://img.youtube.com/vi/${summary.sourceId}/0.jpg" />
-      <meta property="og:url" content="https://share.fusionaivideo.io/summary/${summary._id}" />
+      <meta property="og:url" content="${shareLink}${summary._id}" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="${summary.sourceTitle}" />
       <meta name="twitter:description" content="${summary.summary}" />
       <meta name="twitter:image" content="https://img.youtube.com/vi/${summary.sourceId}/0.jpg" />
-      <link rel="canonical" href="https://share.fusionaivideo.io/summary/${summary._id}" />
+      <link rel="canonical" href="${shareLink}${summary._id}" />
     `;
 }
