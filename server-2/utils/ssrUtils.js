45860986a14a8da8d90da24d7aaf5d10ac0128a5
Jason Chau <what.chau@gmail.com>
2024-07-21 01:19:34 -0400

    update canonical

--- server-2/utils/ssrUtils.js
+++ server-2/utils/ssrUtils.js
@@ -6,11 +6,11 @@ export function generateMetaTags(summary) {
       <meta property="og:title" content="${summary.sourceTitle}" />
       <meta property="og:description" content="${summary.summary}" />
       <meta property="og:image" content="https://img.youtube.com/vi/${summary.sourceId}/0.jpg" />
-      <meta property="og:url" content="https://share.fusionaivideo.io/${summary._id}" />
+      <meta property="og:url" content="https://share.fusionaivideo.io/summary/${summary._id}" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="${summary.sourceTitle}" />
       <meta name="twitter:description" content="${summary.summary}" />
       <meta name="twitter:image" content="https://img.youtube.com/vi/${summary.sourceId}/0.jpg" />
-      <link rel="canonical" href="https://share.fusionaivideo.io/${summary._id}" />
+      <link rel="canonical" href="https://share.fusionaivideo.io/summary/${summary._id}" />
     `;
 }
